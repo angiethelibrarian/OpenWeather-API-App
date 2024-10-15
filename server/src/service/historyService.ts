a95f1c537b133b9ebc8516ebc6bf75510ee085e4
Angelica Mora <angiethelibrarian@gmail.com>
2024-10-15 18:26:35 -0400

    added weatherService code

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const { v4: uuidv4 } = require('uuid');
 
 // TODO: Define a City class with name and id properties
@@ -26,22 +26,33 @@ class HistoryService {
     try {
       await fs.writeFile(this.searchHistoryFilePath, JSON.stringify(cities), "utf-8");
     } catch (error) {
-      
+      console.log(error);
+      return null;
     }
   }
 
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
   async getCities() {
-    const data = await this.read()
-    return JSON.parse(data);
+    const data = await this.read();
+    if (data) {
+      const citiesArray = JSON.parse(data);
+      return citiesArray.map((city: { name: any; id: any; }) => new City(city.name, city.id));
+    }
+    return [];
+  }
   }
   // TODO Define an addCity method that adds a city to the searchHistory.json file
   async addCity(city: string) {
-    let newId = uuidv4();
-
+    const cities = await this.getCities();
+    const newCity = new City(cityName, uuidv4());
+    cities.push(newCity);
+    await this.write(cities);
   }
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
-  async removeCity(id: string) {}
-}
+  async removeCity(id: string) {
+    const cities = await this.getCities();
+    const updatedCities = cities.filter(city => city.id !== id);
+    await this.write(updatedCities);
+  }
 
 export default new HistoryService();
