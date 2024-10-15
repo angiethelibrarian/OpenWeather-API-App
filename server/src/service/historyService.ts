b1aaf5c5bb5a680c2e9b41fda222c410b7c96ed3
Angelica Mora <angiethelibrarian@gmail.com>
2024-10-15 16:47:34 -0400

    defined a mthos from searchHistory json file and class

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -1,11 +1,25 @@
 // TODO: Define a City class with name and id properties
 
+class City {
+  constructor(public name: string, public id: string = uuidv49()) {}
+}
+
 // TODO: Complete the HistoryService class
 class HistoryService {
+  private filePath = './searchHistory.json';
+
+
   // TODO: Define a read method that reads from the searchHistory.json file
-  // private async read() {}
+  private async read(): Promise<City[]> {
+    try {
+      const data = await fs.readFile(this.filePath, 'utf-8');
+      return JSON.parse(data);
+    } catch (error) {
+      return [];
+    }
+  }
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
-  // private async write(cities: City[]) {}
+  private async write(cities: City[]) {}
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
   // async getCities() {}
   // TODO Define an addCity method that adds a city to the searchHistory.json file
@@ -15,3 +29,7 @@ class HistoryService {
 }
 
 export default new HistoryService();
+function uuidv49(): string {
+  throw new Error("Function not implemented.");
+}
+
