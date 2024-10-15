a6b55c633158d837cfcfedb0fd1dbdf3f98595c6
Angelica Mora <angiethelibrarian@gmail.com>
2024-10-15 17:39:03 -0400

    added new code

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -1,35 +1,47 @@
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
 // TODO: Define a City class with name and id properties
-
 class City {
-  constructor(public name: string, public id: string = uuidv49()) {}
+  constructor(name, id) {
+    this.name = name;
+    this.id = id;
+  }
 }
-
 // TODO: Complete the HistoryService class
 class HistoryService {
-  private filePath = './searchHistory.json';
-
+  private searchHistoryFilePath = "./searchHistory.json"
 
   // TODO: Define a read method that reads from the searchHistory.json file
-  private async read(): Promise<City[]> {
+  private async read() {
     try {
-      const data = await fs.readFile(this.filePath, 'utf-8');
-      return JSON.parse(data);
+      return await fs.readFile(this.searchHistoryFilePath, "utf-8");
     } catch (error) {
-      return [];
+      console.log(error);
+      return (null);
     }
   }
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
-  private async write(cities: City[]) {}
+  private async write(cities: City[]) {
+    try {
+      await fs.writeFile(this.searchHistoryFilePath, JSON.stringify(cities), "utf-8");
+    } catch (error) {
+      
+    }
+  }
+
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
-  // async getCities() {}
+  async getCities() {
+    const data = await this.read()
+    return JSON.parse(data);
+  }
   // TODO Define an addCity method that adds a city to the searchHistory.json file
-  // async addCity(city: string) {}
+  async addCity(city: string) {
+    let newId = uuidv4();
+
+  }
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
-  // async removeCity(id: string) {}
+  async removeCity(id: string) {}
 }
 
 export default new HistoryService();
-function uuidv49(): string {
-  throw new Error("Function not implemented.");
-}
-
